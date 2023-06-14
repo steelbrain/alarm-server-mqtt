6058e8020562cba1fb8a00e35065e99d566c51eb
steelbrain <hello@aneesiqbal.ai>
2023-06-14 04:43:00 +0300

    :new: Support more camera models in parser

--- src/parser.ts
+++ src/parser.ts
@@ -1,9 +1,15 @@
-const MAGIC_NUMBER_START = 79
+const MAGIC_NUMBER_START = 32
 const MAGIC_NUMBER_LENGTH = 9
 
 const CAMERA_NAME_TEST = /[a-z0-9]+/i
+const MANUFACTURER_MARKER = 'EZVIZ'
 
 export function getCameraNameFromEvent(payload: Buffer): string | null {
-  const cameraName = payload.toString('utf8', MAGIC_NUMBER_START, MAGIC_NUMBER_START + MAGIC_NUMBER_LENGTH)
+  const startingIndex = payload.indexOf(MANUFACTURER_MARKER)
+  if (startingIndex === -1) {
+    return null
+  }
+  const newPayload = payload.subarray(startingIndex)
+  const cameraName = newPayload.toString('utf8', MAGIC_NUMBER_START, MAGIC_NUMBER_START + MAGIC_NUMBER_LENGTH)
   return CAMERA_NAME_TEST.test(cameraName) ? cameraName : null
 }
